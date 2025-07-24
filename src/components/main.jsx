8fdfdb3ec71b386bad2c3bfe57b4214f7ef8bab9
Ahmad Hammoud <hammoudahmad530@gmail.com>
2025-07-25 00:57:29 +0300

    o

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import '../index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter basename='/LandAdministrationFRONTEND/'>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
 )
